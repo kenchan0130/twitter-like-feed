cc6755120bd859f5968f824281b332d7bb61fc8e
Tadayuki Onishi <tonishi@folio-sec.com>
2020-07-17 16:09:00 +0900

    add logging

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
@@ -34,25 +35,23 @@ type TwitterLikesResponse struct {
 }
 
 func getTwitterLike(username string, lang string) (*[]Tweet, error) {
-	res, err := http.Get(fmt.Sprintf("https://syndication.twitter.com/timeline/likes?lang=%s&screen_name=%s", lang, username))
+	url := fmt.Sprintf("https://syndication.twitter.com/timeline/likes?lang=%s&screen_name=%s", lang, username)
+	res, err := http.Get(url)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("An error occurred while trying to access %s, err: %s", url, err.Error())
 	}
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-		return nil, fmt.Errorf("HTTP status code is %d", res.StatusCode)
+		return nil, fmt.Errorf("In accessing %s, HTTP status code is %d", url, res.StatusCode)
 	}
 
 	var twitterLikesResponse TwitterLikesResponse
 	err = json.NewDecoder(res.Body).Decode(&twitterLikesResponse)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("%s response returned an unexpected JSON. err: %s\n\n%s", url, err.Error(), twitterLikesResponse.Body)
 	}
 
-	doc, err := goquery.NewDocumentFromReader(strings.NewReader(twitterLikesResponse.Body))
-	if err != nil {
-		return nil, fmt.Errorf("Unexpected HTML. err: %s\n\n%s", err.Error(), twitterLikesResponse.Body)
-	}
+	doc, _ := goquery.NewDocumentFromReader(strings.NewReader(twitterLikesResponse.Body))
 
 	var tweetList = make([]Tweet, 0)
 	doc.Find(".timeline-TweetList-tweet").Each(func(i int, s *goquery.Selection) {
@@ -105,6 +104,7 @@ func main() {
 	}
 
 	r := gin.Default()
+
 	r.GET("/", func(c *gin.Context) {
 		c.String(200, "pong")
 	})
@@ -113,12 +113,14 @@ func main() {
 		lang := c.DefaultQuery("lang", "ja")
 		tweetList, err := getTwitterLike(username, lang)
 		if err != nil {
+			log.Fatal(err)
 			c.String(http.StatusInternalServerError, err.Error())
 			return
 		}
 
 		rss, err := generateFeed(username, *tweetList)
 		if err != nil {
+			log.Fatal(err)
 			c.String(http.StatusInternalServerError, err.Error())
 			return
 		}
@@ -126,5 +128,6 @@ func main() {
 		c.Header("Content-Type", "application/xml; charset=utf-8")
 		c.String(http.StatusOK, rss)
 	})
+
 	r.Run(fmt.Sprintf(":%d", port))
 }
